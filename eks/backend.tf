b4440563f87eeae12892c01dde8c3ab076b675f8
Abhishek Pratap Singh <84613735+iabhishekpratap@users.noreply.github.com>
2025-04-14 09:03:33 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,22 @@
 terraform {
   required_version = "~> 1.11.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "ap-south-1"
+    bucket         = "dev-aman-tf-bucket-mumbai"   # 🔁 Must be a real bucket in ap-south-1
+    region         = "ap-south-1"                  # ✅ Correct region
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-Files-Mumbai"           # 🔁 Must exist in ap-south-1
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region  = var.aws-region
+  region = var.aws-region
 }
